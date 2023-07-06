7eb2261c9ad6abb878e118760436bfb9df78146a
jmulliken97 <jmulliken3@gmail.com>
2023-07-06 14:49:51 -0500

    Updates to compare and view
    Now sortable when having json loaded. Compare allows for custom comparison using out inhouse (lol) measurement. Added legend pop out. Started ground work for adding the rushing and receiving stats.

--- GUI.py
+++ GUI.py
@@ -26,6 +26,7 @@ class Ui_MainWindow(object):
         self.label = QtWidgets.QLabel(self.scraping_tab)
         self.label.setGeometry(QtCore.QRect(40, 40, 141, 31))
         self.label.setObjectName("label")
+        
         self.lineEdit = QtWidgets.QLineEdit(self.scraping_tab)
         self.lineEdit.setGeometry(QtCore.QRect(190, 40, 551, 31))
         self.lineEdit.setObjectName("lineEdit")
@@ -37,6 +38,7 @@ class Ui_MainWindow(object):
         self.label_player = QtWidgets.QLabel(self.scraping_tab)
         self.label_player.setGeometry(QtCore.QRect(40, 90, 141, 31))
         self.label_player.setObjectName("label_player")
+        
         self.lineEdit_player = QtWidgets.QLineEdit(self.scraping_tab)
         self.lineEdit_player.setGeometry(QtCore.QRect(190, 90, 551, 31))
         self.lineEdit_player.setObjectName("lineEdit_player")
@@ -74,8 +76,16 @@ class Ui_MainWindow(object):
         self.pushButton_load.setObjectName("pushButton_load")
         
         self.comboBox_year = QtWidgets.QComboBox(self.json_tab)
-        self.comboBox_year.setGeometry(QtCore.QRect(40, 20, 211, 31))
+        self.comboBox_year.setGeometry(QtCore.QRect(40, 20, 75, 31))
         self.comboBox_year.setObjectName("comboBox_year")
+
+        self.comboBox_sort_options = QtWidgets.QComboBox(self.json_tab)
+        self.comboBox_sort_options.setGeometry(QtCore.QRect(130, 20, 100, 31))
+        self.comboBox_sort_options.setObjectName("comboBox_sort_options")
+        
+        self.pushButton_legend = QtWidgets.QPushButton(self.json_tab)
+        self.pushButton_legend.setGeometry(QtCore.QRect(350, 20, 89, 25))
+        self.pushButton_legend.setObjectName("pushButton_legend")
 
         self.pushButton_plot = QtWidgets.QPushButton(self.json_tab)
         self.pushButton_plot.setGeometry(QtCore.QRect(600, 20, 89, 25))
@@ -84,6 +94,16 @@ class Ui_MainWindow(object):
         self.label_filename = QtWidgets.QLabel(self.centralwidget)
         self.label_filename.setGeometry(QtCore.QRect(600, 20, 180, 20))
         self.label_filename.setText("No file loaded.")
+        
+        self.pushButton_compare = QtWidgets.QPushButton(self.json_tab)
+        self.pushButton_compare.setGeometry(QtCore.QRect(700, 20, 89, 25))
+        self.pushButton_compare.setObjectName("pushButton_compare")
+        
+        self.comboBox_sort_order = QtWidgets.QComboBox(self.json_tab)  
+        self.comboBox_sort_order.setGeometry(QtCore.QRect(240, 20, 100, 31)) 
+        self.comboBox_sort_order.addItems(["Descending", "Ascending"])
+        self.comboBox_sort_order.setObjectName("comboBox_sort_order")  
+ 
 
         # add json tab to the tab widget
         self.tabWidget.addTab(self.json_tab, "JSON Viewer")
@@ -101,6 +121,8 @@ class Ui_MainWindow(object):
         self.pushButton_all.setText(_translate("MainWindow", "Scrape All"))
         self.pushButton_load.setText(_translate("MainWindow", "Load JSON"))
         self.pushButton_plot.setText(_translate("MainWindow", "Plot Stats"))
+        self.pushButton_compare.setText(_translate("MainWindow", "Compare"))
+        self.pushButton_legend.setText(_translate("MainWindow", "Legend"))
 
         # Connect button to function
         self.pushButton.clicked.connect(self.scrape_espn)
@@ -109,6 +131,10 @@ class Ui_MainWindow(object):
         self.comboBox_sort_column.currentIndexChanged.connect(self.sort_dataframe)
         self.pushButton_plot.clicked.connect(self.plot_stats)
         self.comboBox_year.currentIndexChanged.connect(self.update_table)
+        self.pushButton_compare.clicked.connect(self.compare_stats)
+        self.comboBox_sort_options.currentIndexChanged.connect(self.sort_dataframe)
+        self.pushButton_legend.clicked.connect(self.show_legend)
+        self.comboBox_sort_order.currentIndexChanged.connect(self.sort_dataframe)
 
     def scrape_espn(self):
         url = self.lineEdit.text()
@@ -126,33 +152,29 @@ class Ui_MainWindow(object):
 
     def load_json_file(self):
         self.data_processor.load_json()
-        print(self.data_processor.data_dict)  # Print the data_dict
         self.comboBox_year.clear()
         self.comboBox_year.addItems(sorted(self.data_processor.data_dict.keys()))
         self.label_filename.setText(f"Loaded file: {os.path.basename(self.data_processor.get_file_name())}")  # Display the loaded file's name
 
         year = self.comboBox_year.currentText()  # Get the selected year
         data_df = self.data_processor.data_dict[year]  # Get the DataFrame for the selected year
-        print(data_df)  # Print the DataFrame
 
-    # Fill the QTableWidget with the data from the DataFrame
+        # Fill the QTableWidget with the data from the DataFrame
         self.tableWidget.setRowCount(len(data_df))
         self.tableWidget.setColumnCount(len(data_df.columns))
         self.tableWidget.setHorizontalHeaderLabels(data_df.columns)
-        print(self.tableWidget.rowCount())
-        print(self.tableWidget.columnCount())
         for i, (index, row) in enumerate(data_df.iterrows()):
             for j, value in enumerate(row):
                 self.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(value)))
-
-
+        self.comboBox_sort_column.clear()
+        self.comboBox_sort_options.addItems([col for col in self.data_processor.get_sortable_columns(year) if col != "Player"])  # Exclude the "Player" column
 
     def sort_dataframe(self):
         year = self.comboBox_year.currentText()
-        sort_by = self.comboBox_sort_column.currentText()
-        sort_order = self.comboBox_sort.currentText()
+        sort_by = self.comboBox_sort_options.currentText()
+        sort_order = self.comboBox_sort_order.currentText() 
         self.data_processor.sort_dataframe(year, sort_by, sort_order)
-        self.update_table()  # Update the table after sorting
+        self.update_table()
         
     def update_table(self):
         year = self.comboBox_year.currentText()
@@ -162,7 +184,88 @@ class Ui_MainWindow(object):
         self.tableWidget.setHorizontalHeaderLabels(data_df.columns)
         for i, (index, row) in enumerate(data_df.iterrows()):
             for j, cell in enumerate(row):
-                self.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(cell)))    
+                self.tableWidget.setItem(i, j, QtWidgets.QTableWidgetItem(str(cell)))
+                
+    def show_legend(self):
+        dialog = QtWidgets.QDialog()
+        dialog.setWindowTitle("Legend")
+        dialog.resize(400, 300)
+
+        text_edit = QtWidgets.QTextEdit(dialog)
+        text_edit.setGeometry(10, 10, 380, 280)
+        text_edit.setReadOnly(True)
+
+        # Display the legend
+        text_edit.append("<b>Pass Yds:</b> Total passing yards")
+        text_edit.append("<b>Yds/Att:</b> Yards per attempt")
+        text_edit.append("<b>Att:</b> Number of attempts")
+        text_edit.append("<b>Cmp:</b> Number of completions")
+        text_edit.append("<b>Cmp %:</b> Completion percentage")
+        text_edit.append("<b>TD:</b> Number of touchdowns")
+        text_edit.append("<b>INT:</b> Number of interceptions")
+        text_edit.append("<b>Rate:</b> Quarterback rating")
+        text_edit.append("<b>1st:</b> Number of first downs")
+        text_edit.append("<b>1st%:</b> First down percentage")
+        text_edit.append("<b>20+:</b> Number of 20+ yards passes")
+        text_edit.append("<b>40+:</b> Number of 40+ yards passes")
+        text_edit.append("<b>Lng:</b> Longest pass")
+        text_edit.append("<b>Sck:</b> Number of sacks")
+        text_edit.append("<b>SckY:</b> Yards lost due to sacks")
+        text_edit.append("<b>TD:INT Ratio:</b> Ratio of touchdowns to interceptions")
+        text_edit.append("<b>ANY/A:</b> Adjusted net yards per pass attempt")
+        
+        text_edit.append("\n<b>Rushing Stats:</b>")
+        text_edit.append("<b>Rush Yds:</b> Total rushing yards")
+        text_edit.append("<b>Att:</b> Number of attempts")
+        text_edit.append("<b>TD:</b> Number of touchdowns")
+        text_edit.append("<b>20+:</b> Number of 20+ yards rushes")
+        text_edit.append("<b>40+:</b> Number of 40+ yards rushes")
+        text_edit.append("<b>Lng:</b> Longest rush")
+        text_edit.append("<b>Rush 1st:</b> Number of first downs")
+        text_edit.append("<b>Rush 1st%:</b> First down percentage")
+        text_edit.append("<b>Rush FUM:</b> Number of fumbles")
+
+        dialog.exec_()
+        
+    def compare_stats(self):
+        year = self.comboBox_year.currentText()
+        players = []
+
+        while True:
+            player, ok1 = QtWidgets.QInputDialog.getItem(None, "Input", "Select a player:", self.data_processor.get_player_names(year), editable=False)
+            if ok1:
+                players.append(player)
+                add_another = QtWidgets.QMessageBox.question(None, 'Question', 'Would you like to add another player?', QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No, QtWidgets.QMessageBox.No)
+                if add_another == QtWidgets.QMessageBox.No:
+                    break
+            else:
+                break
+
+        player_scores = {}
+        for player in players:
+            player_data = self.data_processor.data_dict[year][self.data_processor.data_dict[year]['Player'] == player]
+            if not player_data.empty:
+                player_scores[player] = self.data_processor.calculate_score(player_data.to_dict(orient='records')[0])
+
+        # Sort the players by their scores
+        sorted_players = sorted(player_scores.items(), key=lambda x: x[1], reverse=True)
+
+        # Create a QDialog for the output
+        dialog = QtWidgets.QDialog()
+        dialog.setWindowTitle("Player Comparison")
+        dialog.resize(400, 300)
+
+        # Create a QTextEdit inside the QDialog
+        text_edit = QtWidgets.QTextEdit(dialog)
+        text_edit.setGeometry(10, 10, 380, 280)
+        text_edit.setReadOnly(True)
+
+        # Display the sorted players and their scores
+        for player, score in sorted_players:
+            text_edit.append(f"<b>{player}</b>: {score}")
+
+        dialog.exec_()
+
 
     def plot_stats(self):
         year = self.comboBox_year.currentText()
@@ -170,7 +273,7 @@ class Ui_MainWindow(object):
         stats = []
 
         while True:
-            player, ok1 = QtWidgets.QInputDialog.getItem(None, "Input", "Select a player:", self.data_processor.get_player_names(), editable=False)
+            player, ok1 = QtWidgets.QInputDialog.getItem(None, "Input", "Select a player:", self.data_processor.get_player_names(year), editable=False)
             if ok1:
                 players.append(player)
                 add_another = QtWidgets.QMessageBox.question(None, 'Question', 'Would you like to add another player?', QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No, QtWidgets.QMessageBox.No)
--- data_processor.py
+++ data_processor.py
@@ -9,6 +9,64 @@ class DataProcessor:
         self.data_dict = {}
         self.textEdit = text_edit_widget
         
+    def determine_stats_type(self, stats):
+        passing_headers = ['Player', 'Pass Yds', 'Yds/Att', 'Att', 'Cmp', 'Cmp %', 'TD', 'INT', 'Rate', '1st', '1st%', '20+', '40+', 'Lng', 'Sck', 'SckY']
+        rushing_headers = ['Player', 'Rush Yds', 'Att', 'TD', '20+', '40+', 'Lng', 'Rush 1st', 'Rush 1st%', 'Rush FUM']
+        receiving_headers = ['Player', 'Rec', 'Yds', 'TD', '20+', '40+', 'LNG', 'Rec 1st', '1st%', 'Rec FUM', 'Rec YAC/R', 'Tgts']
+
+        stats_keys = stats.keys()
+
+        if all(item in stats_keys for item in passing_headers):
+            return "passing"
+        elif all(item in stats_keys for item in rushing_headers):
+            return "rushing"
+        elif all(item in stats_keys for item in receiving_headers):
+            return "receiving"
+        else:
+            return "unknown"
+
+    def calculate_score(self, player, stats_type):
+        if stats_type == "passing":
+            weights = {
+                "Pass Yds": 0.05,
+                "Yds/Att": 0.15,
+                "Cmp %": 0.15,
+                "TD:INT Ratio": 0.2,
+                "Rate": 0.05,
+                "Sck": -0.05,
+                "SckY": -0.05,
+                "ANY/A": 0.2,
+            }
+
+            # Calculate the touchdown-to-interception ratio
+            if player["INT"] != 0:
+                player["TD:INT Ratio"] = round(player["TD"] / player["INT"], 2)
+            else:
+                # If no interceptions,use the number of touchdowns
+                player["TD:INT Ratio"] = round(player["TD"], 2)
+
+            # Calculate the Adjusted Net Yards per Attempt
+            player["ANY/A"] = round((player["Pass Yds"] + 20 * player["TD"] - 45 * player["INT"] - player["SckY"]) / (player["Att"] + player["Sck"]), 2)
+
+        elif stats_type == "rushing":
+            weights = {
+                "Rush Yds": 0.05,
+                "Att": 0.15,
+                "TD": 0.2,
+                "20+": 0.05,
+                "40+": 0.05,
+                "Lng": 0.2,
+                "Rush 1st": 0.05,
+                "Rush 1st%": 0.15,
+                "Rush FUM": -0.1,
+            }
+
+        score = 0
+        for stat, weight in weights.items():
+            score += player[stat] * weight
+
+        return score
+
     def flatten(self, d, parent_key='', sep='_'):
         items = []
         for k, v in d.items():
@@ -37,7 +95,14 @@ class DataProcessor:
             with open(self.file_name, 'r') as json_file:
                 data = json.load(json_file)
                 for year, year_data in data.items():
+                    for player, stats in year_data.items():
+                        stats_type = self.determine_stats_type(stats)
+                        if stats_type != "unknown":
+                            self.calculate_score(stats, stats_type)
                     self.data_dict[year] = pd.DataFrame.from_records(list(year_data.values()))
+            # Save the updated data back to the file
+            with open(self.file_name, 'w') as json_file:
+                json.dump(data, json_file)
 
     def get_file_name(self):
         return self.file_name if hasattr(self, 'file_name') else 'No file loaded.'
@@ -52,15 +117,30 @@ class DataProcessor:
             return self.data_dict[year].columns.tolist()
         return []
     
-    def sort_dataframe(self, year, sort_by, sort_order):
-        if year in self.data_dict and sort_order != "No Sort":
-            self.data_dict[year].sort_values(by=sort_by, axis=0, ascending=(sort_order == "Ascending"), inplace=True)
-            
+    def sort_dataframe(self, year, sort_by, sort_order): 
+        df = self.data_dict[year]
+        if sort_order == "Ascending":
+            self.data_dict[year] = df.sort_values(by=sort_by)
+        else:
+            self.data_dict[year] = df.sort_values(by=sort_by, ascending=False)  
+ 
     def get_sortable_columns(self, year):
         if year in self.data_dict:
             return self.data_dict[year].columns.tolist()
         return []
-
+    
+    def compare_stats(self, year, stats, players):
+        comparison_results = ""
+        for player in players:
+            player_data = self.data_dict[year][self.data_dict[year]['Player'] == player]
+            if not player_data.empty:
+                comparison_results += f"Stats for {player}:\n"
+                for stat in stats:
+                    stat_value = player_data[stat].values[0]
+                    comparison_results += f"{stat}: {stat_value}\n"
+                comparison_results += "\n"
+        return comparison_results
+    
     def plot_stats(self, year, stat_columns, player_names):
         if year in self.data_dict:
             bar_width = 0.35
